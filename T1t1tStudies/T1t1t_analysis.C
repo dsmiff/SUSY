f281aec35419ed40a65d93feecc7f25b9704cf2e
Dominic Smith <dominic.smith@bristol.ac.uk>
2014-04-29 11:43:55 +0100

    final changes

--- T1t1tStudies/T1t1t_analysis.C
+++ T1t1tStudies/T1t1t_analysis.C
@@ -1,3 +1,14 @@
+// Author: Dominic Smith 
+// Date: April 2014
+// University of Bristol/ Vrije Universitait Brussel
+// 
+// Description: 
+// ROOT script to plot several histograms on a single and multiple pdfs/ps.
+// Requires a ROOT TFile with the correct histo titles.
+// User can choose what style to plot histos with
+
+
+
 void T1t1t_analysis()
 {
 
@@ -12,12 +23,10 @@ void T1t1t_analysis()
   std::cout << "Rootfile : " << rootname << std::endl;
 
   // For TH2D histograms
-  
   TString plots2 = "DeltaR1,DeltaR2,DeltaR3,DeltaR4";
   TObjArray* tokens1 = plots2.Tokenize(",");
   int n1 = tokens1->GetEntries();
   
-
   // For TH1D histograms
   TString plots = "JetPT,1stJetPT,2ndJetPT,JetEta,1stJetMass,2ndJetMass,HT,NJets,TopPT,StopPT,LSPPT";
   TObjArray* tokens = plots.Tokenize(",");
@@ -25,7 +34,7 @@ void T1t1t_analysis()
 
   // Canvas initialising 
   TCanvas c1("c1","draw plots",0,0,1000, 800);
-  TCanvas ci_1("ci_1","draw shit",0,0,1000, 800);
+  TCanvas ci_1("ci_1","draw separately",0,0,1000, 800);
   TCanvas c2("c2","draw together", 0, 0, 1000, 800);
   c1.Divide( 4, TMath::Nint(n/3.));
 
@@ -58,51 +67,42 @@ void T1t1t_analysis()
       h2i->Draw(style);
       c2.Print("histos.pdf(");
 
-
     }
   
 
   c1.cd();
   c1.SaveAs("histDraws.ps");
 
-
-
-
   // Plot TH1D histos
 
   for(int i=0; i<n; i++)
     {
 
-  TString style = ((TObjString*) styletokens->At(0))->String();
-  TString plot = ((TObjString*) tokens->At(i))->String();
+      TString style = ((TObjString*) styletokens->At(0))->String();
+      TString plot = ((TObjString*) tokens->At(i))->String();
 
-  std::cout << "Plot " << plot << std::endl;
-  TH1F *hi = (TH1F*)rootfile->Get(plot);
+      std::cout << "Plot " << plot << std::endl;
+      TH1F *hi = (TH1F*)rootfile->Get(plot);
 
-  ci_1.cd();
-  hi->Draw(style);
-  hi->Scale(1/hi->GetEntries());
-  hi->GetXaxis()->SetTitle((const Char_t*) (plot));
-  hi->GetYaxis()->SetTitle("Events");
-  ci_1.SaveAs((const Char_t*) (plot + ".pdf"));
+      ci_1.cd();
+      hi->Draw(style);
+      hi->Scale(1/hi->GetEntries());
+      hi->GetXaxis()->SetTitle((const Char_t*) (plot));
+      hi->GetYaxis()->SetTitle("Events");
+      ci_1.SaveAs((const Char_t*) (plot + ".pdf"));
 
-  c1.cd(i+1);
-  hi->Draw(style);
+      c1.cd(i+1);
+      hi->Draw(style);
 
 
-  c2.cd();
-  hi->Draw(style);
-  c2.Print("histos.pdf(");
+      c2.cd();
+      hi->Draw(style);
+      c2.Print("histos.pdf(");
 
     }	   
 
   c2.Print("histos.pdf)");
   c1.cd();
   c1.SaveAs("histDraws.ps");
-
-
-      
-
-  
 
 }
